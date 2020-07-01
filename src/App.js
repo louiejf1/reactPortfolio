5f970d3103d05d6d7bf7443d7cf12bc583a75b07
ludwig Flores <louiejf1@gmail.com>
2020-07-01 15:47:29 -0700

    Basic Site Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Components/Pages/Home";
+import About from "./Components/Pages/About";
+import Projects from "./Components/Pages/Projects";
+import Contact from "./Components/Pages/Contact";
+import NoMatch from "./Components/Pages/NoMatch";
+import MainLayout from "./Components/Layout/MainLayout";
+import NavBar from "./Components/Layout/NavBar";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NavBar/>
+      <MainLayout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </MainLayout>
+    </React.Fragment>
   );
 }
 
